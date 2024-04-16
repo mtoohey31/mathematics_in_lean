523781e9dcfb7294e60d590fc4bd79b055334a3a
Matthew Toohey <contact@mtoohey.com>
2024-04-16 15:54:18 -0400

    C03/S05

--- MIL/C03_Logic/S05_Disjunction.lean
+++ MIL/C03_Logic/S05_Disjunction.lean
@@ -59,19 +59,57 @@ example : x < |y| → x < y ∨ x < -y := by
 namespace MyAbs
 
 theorem le_abs_self (x : ℝ) : x ≤ |x| := by
-  sorry
+  rcases le_or_gt 0 x with h | h
+  · rw [abs_of_nonneg h]
+  · rw [abs_of_neg h]
+    linarith
 
 theorem neg_le_abs_self (x : ℝ) : -x ≤ |x| := by
-  sorry
+  rcases le_or_gt 0 x with h | h
+  · rw [abs_of_nonneg h]
+    linarith
+  · rw [abs_of_neg h]
 
 theorem abs_add (x y : ℝ) : |x + y| ≤ |x| + |y| := by
-  sorry
+  rcases le_or_gt 0 (x + y) with h | h
+  · rw [abs_of_nonneg h]
+    apply add_le_add (le_abs_self x) (le_abs_self y)
+  · rw [abs_of_neg h]
+    rw [neg_add]
+    apply add_le_add (neg_le_abs_self x) (neg_le_abs_self y)
 
 theorem lt_abs : x < |y| ↔ x < y ∨ x < -y := by
-  sorry
+  constructor
+  · rcases le_or_gt 0 y with h | h
+    · rw [abs_of_nonneg h]
+      exact Or.inl
+    · rw [abs_of_neg h]
+      exact Or.inr
+  · rcases le_or_gt 0 y with h | h
+    · rw [abs_of_nonneg h]
+      rintro (h' | h')
+      · exact h'
+      · linarith
+    · rw [abs_of_neg h]
+      rintro (h' | h')
+      · linarith
+      · exact h'
 
 theorem abs_lt : |x| < y ↔ -y < x ∧ x < y := by
-  sorry
+  constructor
+  · rcases le_or_gt 0 x with h | h
+    · rw [abs_of_nonneg h]
+      intro h'
+      constructor <;> linarith
+    · rw [abs_of_neg h]
+      intro h'
+      constructor <;> linarith
+  · rcases le_or_gt 0 x with h | h
+    · rw [abs_of_nonneg h]
+      exact And.right
+    · rw [abs_of_neg h]
+      intro h'
+      linarith
 
 end MyAbs
 
@@ -92,23 +130,73 @@ example {m n k : ℕ} (h : m ∣ n ∨ m ∣ k) : m ∣ n * k := by
     apply dvd_mul_right
 
 example {z : ℝ} (h : ∃ x y, z = x ^ 2 + y ^ 2 ∨ z = x ^ 2 + y ^ 2 + 1) : z ≥ 0 := by
-  sorry
+  rcases h with ⟨a, b, h | h⟩; repeat linarith [pow_two_nonneg a, pow_two_nonneg b]
 
 example {x : ℝ} (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have h1 : x ^ 2 = (x + 1) * (x - 1) + 1 := by ring
+  have h2 : (x + 1) * (x - 1) = 0 := by
+    rw [<- add_left_inj 1, zero_add, <- h1]
+    exact h
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h2 with h3 | h3
+  . right
+    rw [<- add_left_inj 1]
+    simp
+    exact h3
+  . left
+    rw [<- add_left_inj 1] at h3
+    simp at h3
+    exact h3
 
 example {x y : ℝ} (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have h1 : x ^ 2 - y ^ 2 = (x + y) * (x - y) := by ring
+  have h2 : (x + y) * (x - y) = 0 := by
+    rw [<- h1, <- add_left_inj (y ^ 2)]
+    simp
+    exact h
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h2 with h3 | h3
+  . right
+    rw [<- add_left_inj y]
+    simp
+    exact h3
+  . left
+    rw [<- add_left_inj y] at h3
+    simp at h3
+    exact h3
 
 section
 variable {R : Type*} [CommRing R] [IsDomain R]
 variable (x y : R)
 
 example (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have h1 : x ^ 2 = (x + 1) * (x - 1) + 1 := by ring
+  have h2 : (x + 1) * (x - 1) = 0 := by
+    rw [<- add_left_inj 1, zero_add, <- h1]
+    exact h
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h2 with h3 | h3
+  . right
+    rw [<- add_left_inj 1]
+    simp
+    exact h3
+  . left
+    rw [<- add_left_inj 1] at h3
+    simp at h3
+    exact h3
 
 example (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have h1 : x ^ 2 - y ^ 2 = (x + y) * (x - y) := by ring
+  have h2 : (x + y) * (x - y) = 0 := by
+    rw [<- h1, <- add_left_inj (y ^ 2)]
+    simp
+    exact h
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h2 with h3 | h3
+  . right
+    rw [<- add_left_inj y]
+    simp
+    exact h3
+  . left
+    rw [<- add_left_inj y] at h3
+    simp at h3
+    exact h3
 
 end
 
@@ -125,5 +213,14 @@ example (P : Prop) : ¬¬P → P := by
   contradiction
 
 example (P Q : Prop) : P → Q ↔ ¬P ∨ Q := by
-  sorry
+  constructor
+  . intro h
+    by_cases h' : P
+    . right
+      exact h h'
+    . exact Or.inl h'
+  . intro h p
+    rcases h with h' | h'
+    . contradiction
+    . exact h'
 
