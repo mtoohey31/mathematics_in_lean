f09373900c3312f8dd9a2a9ca67770290756725f
Matthew Toohey <contact@mtoohey.com>
2024-05-03 19:18:47 -0400

    C04/S03

--- MIL/C04_Sets_and_Functions/S03_The_Schroeder_Bernstein_Theorem.lean
+++ MIL/C04_Sets_and_Functions/S03_The_Schroeder_Bernstein_Theorem.lean
@@ -28,10 +28,12 @@ theorem sb_right_inv {x : α} (hx : x ∉ sbSet f g) : g (invFun g x) = x := by
     rw [sbSet, mem_iUnion]
     use 0
     rw [sbAux, mem_diff]
-    sorry
+    exact ⟨trivial, hx⟩
   have : ∃ y, g y = x := by
-    sorry
-  sorry
+    have ⟨y, _, gyx⟩ := this
+    exact ⟨y, gyx⟩
+  apply invFun_eq
+  trivial
 
 theorem sb_injective (hf : Injective f) : Injective (sbFun f g) := by
   set A := sbSet f g with A_def
@@ -50,15 +52,23 @@ theorem sb_injective (hf : Injective f) : Injective (sbFun f g) := by
       rw [if_pos x₁A, if_neg x₂nA] at hxeq
       rw [A_def, sbSet, mem_iUnion] at x₁A
       have x₂eq : x₂ = g (f x₁) := by
-        sorry
+        rw [←sb_right_inv f g x₂nA]
+        congr
+        exact symm (hxeq)
       rcases x₁A with ⟨n, hn⟩
       rw [A_def, sbSet, mem_iUnion]
       use n + 1
       simp [sbAux]
       exact ⟨x₁, hn, x₂eq.symm⟩
-    sorry
-  push_neg  at xA
-  sorry
+    rw [if_pos x₁A, if_pos x₂A] at hxeq
+    exact hf hxeq
+  push_neg at xA
+  rw [if_neg xA.left, if_neg xA.right] at hxeq
+  calc
+    x₁ = g (invFun g x₁) := symm (sb_right_inv f g xA.left)
+    _  = g (invFun g x₂) := by
+      congr
+    _ = x₂ := sb_right_inv f g xA.right
 
 theorem sb_surjective (hf : Injective f) (hg : Injective g) : Surjective (sbFun f g) := by
   set A := sbSet f g with A_def
@@ -77,7 +87,10 @@ theorem sb_surjective (hf : Injective f) (hg : Injective g) : Surjective (sbFun 
       exact ⟨n, xmem⟩
     simp only [h_def, sbFun, if_pos this]
     exact hg hx
-  sorry
+  use g y
+  have : ∃ x, g x = g y := by use y
+  apply hg
+  rw [h_def, sbFun, if_neg gyA, invFun, dif_pos, this.choose_spec]
 
 end
 
